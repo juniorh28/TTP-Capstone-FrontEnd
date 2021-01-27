45218c84070c2e61620ce7244760b7998ebea0aa
Anastasiya Chabotska <anc33139@gmail.com>
2021-01-27 10:27:27 -0500

    material ui for homepage

--- src/components/AllPlaces/containers/HomeContainer.jsx
+++ src/components/AllPlaces/containers/HomeContainer.jsx
@@ -1,40 +1,169 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Link } from "react-router-dom";
 import RoutesContainer from "../routes/RoutesContainer";
+import Button from "@material-ui/core/Button";
+import AppBar from "@material-ui/core/AppBar";
+import ToolBar from "@material-ui/core/Toolbar";
+import IconButton from "@material-ui/core/IconButton";
+import Grid from "@material-ui/core/Grid";
+import { makeStyles, Typography } from "@material-ui/core";
+import MuseumRoundedIcon from "@material-ui/icons/MuseumRounded";
+import RestaurantRoundedIcon from "@material-ui/icons/RestaurantRounded";
+import LocalFloristRoundedIcon from "@material-ui/icons/LocalFloristRounded";
+
+const styles = {
+  homepage: {
+    height: 1356,
+  },
+  mainSection: {
+    align: "center",
+  },
+  section: {
+    alignItems: "center",
+  },
+  button: {
+    margin: "10px",
+    size: "large",
+  },
+  container: {
+    display: "flex",
+    alignItems: "space-between",
+  },
+};
 
 class HomeContainer extends Component {
   render() {
     return (
       <div>
-        <Link to="/all">
-          <button>All Places in NYC</button>
-        </Link>
-        <br />
-        <Link to="/categories/art">
-          <button>Art</button>
-        </Link>
-        <Link to="/categories/food">
-          <button>Food</button>
-        </Link>
-        <Link to="/categories/parks">
-          <button>Parks</button>
-        </Link>
-        <br />
-        <Link to="/boroughs/brooklyn">
-          <button>Brooklyn</button>
-        </Link>
-        <Link to="/boroughs/manhattan">
-          <button>Manhattan</button>
-        </Link>
-        <Link to="/boroughs/queens">
-          <button>Queens</button>
-        </Link>
-        <Link to="/boroughs/bronx">
-          <button>The Bronx</button>
-        </Link>
-        <Link to="/boroughs/statenisland">
-          <button>Staten Island</button>
-        </Link>
+        <AppBar>
+          <ToolBar>
+            <Typography variant="h4">Yorkify</Typography>
+            <Button variant="contained">Login</Button>
+            <Button variant="contained">Sign Up</Button>
+          </ToolBar>
+        </AppBar>
+        <div className="homepage" style={styles.homepage}>
+          <Typography variant="h2">Best Places in NYC</Typography>
+          <Grid
+            container
+            spacing={6}
+            direction="column"
+            alignItems="center"
+            justify="center"
+          >
+            <Grid item>
+              <Typography variant="h4" align="center">
+                View
+              </Typography>
+              <Link to="/all">
+                <Button
+                  variant="contained"
+                  color="primary"
+                  style={styles.button}
+                  size="large"
+                >
+                  All Places in NYC
+                </Button>
+              </Link>
+            </Grid>
+
+            <Grid item style={styles.section}>
+              <Typography variant="h4" align="center">
+                Categories
+              </Typography>
+
+              <Link to="/categories/art">
+                <Button
+                  variant="contained"
+                  color="primary"
+                  style={styles.button}
+                  size="large"
+                  startIcon={<MuseumRoundedIcon />}
+                >
+                  Art
+                </Button>
+              </Link>
+              <Link to="/categories/food">
+                <Button
+                  variant="contained"
+                  color="primary"
+                  style={styles.button}
+                  size="large"
+                  startIcon={<RestaurantRoundedIcon />}
+                >
+                  Food
+                </Button>
+              </Link>
+              <Link to="/categories/parks">
+                <Button
+                  variant="contained"
+                  color="primary"
+                  style={styles.button}
+                  size="large"
+                  startIcon={<LocalFloristRoundedIcon />}
+                >
+                  Parks
+                </Button>
+              </Link>
+            </Grid>
+
+            <Grid item>
+              <Typography variant="h4" align="center">
+                Boroughs
+              </Typography>
+              <Link to="/boroughs/brooklyn">
+                <Button
+                  variant="contained"
+                  color="primary"
+                  style={styles.button}
+                  size="large"
+                >
+                  Brooklyn
+                </Button>
+              </Link>
+              <Link to="/boroughs/manhattan">
+                <Button
+                  variant="contained"
+                  color="primary"
+                  style={styles.button}
+                  size="large"
+                >
+                  Manhattan
+                </Button>
+              </Link>
+              <Link to="/boroughs/queens">
+                <Button
+                  variant="contained"
+                  color="primary"
+                  style={styles.button}
+                  size="large"
+                >
+                  Queens
+                </Button>
+              </Link>
+              <Link to="/boroughs/bronx">
+                <Button
+                  variant="contained"
+                  color="primary"
+                  style={styles.button}
+                  size="large"
+                >
+                  The Bronx
+                </Button>
+              </Link>
+              <Link to="/boroughs/statenisland">
+                <Button
+                  variant="contained"
+                  color="primary"
+                  style={styles.button}
+                  size="large"
+                >
+                  Staten Island
+                </Button>
+              </Link>
+            </Grid>
+          </Grid>
+        </div>
       </div>
     );
   }
--- src/components/AllPlaces/views/AllPlacesView.jsx
+++ src/components/AllPlaces/views/AllPlacesView.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import "./styles/AllPlayersView.css";
 import { Link } from "react-router-dom";
+import Grid from "@material-ui/core/Grid";
+
 // import PropTypes from 'prop-types';
 
 const AllPlacesView = (props) => {
@@ -11,18 +13,23 @@ const AllPlacesView = (props) => {
       <Link to="/">
         <button>Home</button>
       </Link>
-      <h1>{props.id.toUpperCase()}</h1>
-      {props.allPlaces.map((place) => (
-        <div key={place.id}>
-          <img src={place.imageUrl} alt="place image" width="50px" />
-          <h2>{place.name}</h2>
-          <p>{place.description}</p>
-          <p>{place.address}</p>
-          <p>{place.borough}</p>
-          <p>{place.category}</p>
-          <p>{place.numOfLikes}</p>
-        </div>
-      ))}
+
+      <Grid container direction="row" alignItems="flex-start" justify="center">
+        <h1>{props.id.toUpperCase()}</h1>
+        {props.allPlaces.map((place) => (
+          <div key={place.id}>
+            <Grid item xs={2}>
+              <img src={place.imageUrl} alt="place image" width="200px" />
+              <h2>{place.name}</h2>
+              <p>{place.description}</p>
+              <p>{place.address}</p>
+              <p>{place.borough}</p>
+              <p>{place.category}</p>
+              <p>{place.numOfLikes}</p>
+            </Grid>
+          </div>
+        ))}
+      </Grid>
       {props.allPlaces.length === 0 ? <p>No Places Found</p> : ""}
     </div>
     //--------------------//
