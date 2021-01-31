2ed98e7a166f18b267365ba74a80ba0029de87e2
juniorh28 <juniorh2897@gmail.com>
2021-01-31 02:53:14 -0500

    delete junior navbar for a better yorkify navbar, tried changes to routes, dont work but error on submission, check redux

--- src/components/AllPlaces/containers/HeaderContainer.jsx
+++ src/components/AllPlaces/containers/HeaderContainer.jsx
@@ -1,18 +1,18 @@
-/* import React, { Component } from "react";
+import React, { Component } from "react";
 import { Navbar, Nav } from "react-bootstrap";
-
+import { Link } from "react-router-dom";
 class HeaderContainer extends Component {
   render() {
     return (
       <div>
         <Navbar sticky="top" className="navbar">
-          <Navbar.Brand href="#/">Yorkify</Navbar.Brand>
+          <Navbar.Brand href="/">Yorkify</Navbar.Brand>
             <Nav className="justify-content-end">
                 <Nav.Item className="signup">
-                  <Nav.Link style={{ textDecoration: "none", color: "#8ffcff" }} href="#signUp">Sign Up</Nav.Link>
+                  <Nav.Link style={{ textDecoration: "none", color: "#8ffcff" }} href="/Register">Sign Up</Nav.Link>
                 </Nav.Item>
                 <Nav.Item className="login">
-                  <Nav.Link style={{ textDecoration: "none", color: "#8ffcff" }} href="#login">Login</Nav.Link>
+                  <Nav.Link style={{ textDecoration: "none", color: "#8ffcff" }} href="/login">Login</Nav.Link>
                 </Nav.Item>
             </Nav>
         </Navbar>
@@ -21,4 +21,4 @@ class HeaderContainer extends Component {
   }
 }
 
-export default HeaderContainer; */
+export default HeaderContainer; 
